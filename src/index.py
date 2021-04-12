e8aa65e6da565911729bc18020c89961f506124b
hilliaho <taina.hilliaho@helsinki.fi>
2021-04-12 14:48:03 +0300

    testi

--- src/index.py
+++ src/index.py
@@ -1,1 +1,12 @@
-print("testi")
+import pygame
+
+pygame.init()
+naytto = pygame.display.set_mode((640, 480))
+
+naytto.fill((0,0,0))
+pygame.display.flip()
+
+while True:
+    for tapahtuma in pygame.event.get():
+        if tapahtuma.type == pygame.QUIT:
+            exit()
