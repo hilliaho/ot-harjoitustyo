e56aee091b4d3d1d6ee46dba62e827c04bc90e70
hilliaho <taina.hilliaho@helsinki.fi>
2021-04-12 19:37:14 +0300

    toimintaa lisätty

--- src/index.py
+++ src/index.py
@@ -1,13 +1,29 @@
 import pygame
+import os
 
 pygame.init()
-pygame.display.set_caption("Tetris")
 display = pygame.display.set_mode((300, 500))
+pygame.display.set_caption("Tetris")
 
-display.fill((0,0,0))
-pygame.display.flip()
+dirname = os.path.dirname(__file__)
+tetromino = pygame.image.load(
+            os.path.join(dirname, "assets", "square.png")
+        )
+
+x = 0
+y = 0
+clock = pygame.time.Clock()
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+
+    display.fill((0,0,0))
+    display.blit(tetromino, (x, y))
+    pygame.display.flip()
+
+    y += 1
+    clock.tick(60)
