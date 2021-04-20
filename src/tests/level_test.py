b5535fcd67ae2b92122ef5de8cfafb5a5579c20d
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2021-04-20 12:14:18 +0300

    testejä korjattu

--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from level import Level
 
-LEVEL_MAP_ =   [[1, 1, 1, 1, 1, 1],
+LEVEL_MAP =   [[1, 1, 1, 1, 1, 1],
                 [1, 0, 0, 0, 0, 1],
                 [1, 0, 0, 0, 0, 1],
                 [1, 0, 0, 0, 0, 1],
@@ -10,14 +10,14 @@ LEVEL_MAP_ =   [[1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1]]
 
 CELL_SIZE = 25
-
+SPEED = 100
 
 class TestLevel(unittest.TestCase):
     def setUp(self):
         self.level = Level(LEVEL_MAP, CELL_SIZE)
 
     def test_tetromino_can_move(self):
-        tetromino = self.level.tetromino()
+        tetromino = self.level.tetromino(SPEED)
 
         self.assertEqual(tetromino.rect.x, len(LEVEL_MAP[0])*CELL_SIZE//2)
 
